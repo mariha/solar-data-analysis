0e36fcd6cf3e9850c5dc32ea32a51f5d68506d9c
marycha <m.kozinska@gmail.com>
2016-09-16 18:51:22 -0500

    Code quality improvements

--- run.R
+++ run.R
@@ -1,3 +1,4 @@
 source("solar.R")
 
-calculateIrradiance(getwd(), TRUE)
+#calculateIrradiance(workingDir=getwd(), dataFilePattern="produkt_.*\\.txt$", unzipData=FALSE)
+calculateIrradiance(workingDir=getwd(), dataFilePattern="produkt_.*03987\\.txt$", unzipData=FALSE)  # Berlin - Potsdam
